73aaed8a3d0c916951b4f08ed91834491e28336b
harrisonokoth <168322645+harrisonokoth@users.noreply.github.com>
2024-05-24 20:37:14 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+# Asking for user input
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your location: ")
+
+# Printing out a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+
+
+# In[ ]:
+
+
+
+
